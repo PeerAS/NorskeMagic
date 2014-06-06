1d872a88f48e697a9f328b423c6dc3ef3d0166cd
Adhenedhel <peerandreas@live.com>
2014-06-06 20:46:43 +0200

    First commit I think

---
+++ NoMagic/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using NoMagic.Resources;
+
+namespace NoMagic
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
